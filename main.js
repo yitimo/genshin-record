65d1fd45904cb7810c5c4a31240779413d19545e
yitimo <admin@yitimo.com>
2022-11-18 16:29:22 +0800

    UPDATE

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const path = require('path')
 const chalk = require('chalk')
 
-const version = 1
+const version = 2
 const filename = path.resolve(__dirname, `data-${version}.json`)
 
 main()
